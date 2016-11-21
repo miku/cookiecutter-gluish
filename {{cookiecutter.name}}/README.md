dfc72a98c43d4ced7baa967bb5111372d42813ad
Martin Czygan <martin.czygan@gmail.com>
2016-11-21 17:21:23 +0100

    two step sample task

--- {{cookiecutter.name}}/README.md
+++ {{cookiecutter.name}}/README.md
@@ -6,8 +6,9 @@ Data pipeline project scaffold.
 ```
 $ mkvirtualenv myproj
 (myproj) $ python setup.py develop
-(myproj) $ {{ cookiecutter.name }}-build Hello --local-scheduler
+(myproj) $ {{ cookiecutter.name }}-build Analysis --local-scheduler
 ...
+(myproj) $ tree {{ cookiecutter.name }}/derived
 ```
 
 TODO: Add more tasks.
--- {{cookiecutter.name}}/{{ cookiecutter.name }}/tasks/sample.py
+++ {{cookiecutter.name}}/{{ cookiecutter.name }}/tasks/sample.py
@@ -1,13 +1,31 @@
 # coding: utf-8
 
 from {{ cookiecutter.name}}.task import Task
+import luigi
 
 class Hello(Task):
     """
     A sample task.
     """
     def run(self):
-        print("Hello Pipes!")
+        with self.output().open('w') as output:
+            output.write("Hello Pipes!\n")
+            output.write("Second line!\n")
+
+    def output(self):
+        return luigi.LocalTarget(path=self.path())
+
+class Analysis(Task):
+    """
+    Sample task.
+    """
+    def requires(self):
+        return Hello()
+
+    def run(self):
+        with self.input().open() as handle:
+            for line in handle:
+                print('%s: %s' % (line, len(line)))
 
     def complete(self):
         return False
