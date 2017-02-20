8987af28613748b7b4fcb9e1e723c9a1da60780a
Martin Czygan <martin.czygan@gmail.com>
2017-02-20 13:11:21 +0100

    require luigi 2.5.0

--- {{cookiecutter.name}}/setup.py
+++ {{cookiecutter.name}}/setup.py
@@ -3,7 +3,7 @@
 
 import sys
 
-from {{ cookiecutter.name }} import __version__
+from {{cookiecutter.name}} import __version__
 
 if sys.version_info < (2, 7) or sys.version_info.major > 2:
     print("siskin runs with Python 2.7 or higher, but not with Python 3 yet")
@@ -15,6 +15,9 @@ except:
     from distutils.core import setup
 
 install_requires = [
+    # As soon as https://github.com/spotify/luigi/pull/2033 is merged,
+    # use 'luigi>=2.2' again.
+    'luigi==2.5.0',
     'gluish>=0.2.7',
 ]
 
@@ -24,16 +27,16 @@ setup(name='{{ cookiecutter.name }}',
       author='{{ cookiecutter.author }}',
       author_email='{{ cookiecutter.author_email }}',
       packages=[
-        '{{ cookiecutter.name }}',
+          '{{ cookiecutter.name }}',
       ],
       package_dir={'{{ cookiecutter.name }}': '{{ cookiecutter.name }}'},
       package_data={'{{ cookiecutter.name }}': ['assets/*']},
       scripts=[
-        'bin/{{ cookiecutter.name }}-build',
-        'bin/{{ cookiecutter.name }}-ls',
-        'bin/{{ cookiecutter.name }}-output',
-        'bin/{{ cookiecutter.name }}-rm',
+          'bin/{{ cookiecutter.name }}-build',
+          'bin/{{ cookiecutter.name }}-ls',
+          'bin/{{ cookiecutter.name }}-output',
+          'bin/{{ cookiecutter.name }}-rm',
       ],
       install_requires=install_requires,
       zip_safe=False,
-)
+      )
